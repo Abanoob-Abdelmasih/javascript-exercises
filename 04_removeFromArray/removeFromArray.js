3f8aac55bfc27e20d8674708cc3c90121c7dcb37
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-03 21:38:35 +0300

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (a, ...b) {
+  for (let i = 0; i < b.length; i++) {
+    if (a.includes(b[i])) {
+      a.splice(a.indexOf(b[i]), 1);
+    }
+  }
+  return a;
 };
 
 // Do not edit below this line
