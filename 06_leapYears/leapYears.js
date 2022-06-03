0fed3c4558731a17d2d474559cce5f36b870225f
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-03 22:37:55 +0300

    solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (num) {
+  if ((0 == num % 4) && (0 != num % 100) || (0 == num % 400)) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400))
