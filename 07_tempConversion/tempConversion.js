cd2f8758756f54be436a0e5397bc1f364fac92e8
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-03 23:01:10 +0300

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  // let c = Math.ceil(((f - 32) * 5) / 9);
+  let c = Math.round((((f - 32) * 5) / 9) * 10) / 10;
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let f = Math.round((c * (9 / 5) + 32) * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
