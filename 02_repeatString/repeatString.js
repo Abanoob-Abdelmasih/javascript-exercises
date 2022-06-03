04934f233ce1b0002a570e4287023853c3772c23
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-03 21:41:09 +0300

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, n) {
+  let text = "";
+  if (n >= 0) {
+    for (let i = 0; i < n; i++) {
+      text += string;
+    }
+    return text;
+  } else return "ERROR";
 };
 
 // Do not edit below this line
