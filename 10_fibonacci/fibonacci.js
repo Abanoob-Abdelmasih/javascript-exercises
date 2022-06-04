9c75d86b2ef9233c22e6a71a651a1e3980d33f61
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-04 17:36:57 +0300

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (len) {
+  if (len < 1) return "OOPS";
+  let num1 = 1,
+    num2 = 1,
+    next,
+    count = 2,
+    result = [1, 1];
 
+  while (count < len) {
+    next = num1 + num2;
+    num1 = num2;
+    num2 = next;
+    result.push(next);
+    count++;
+  }
+  return result[len - 1];
 };
 
 // Do not edit below this line
