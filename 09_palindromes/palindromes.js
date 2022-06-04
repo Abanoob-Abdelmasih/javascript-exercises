71cf99f8b885a3833183f51a75070ac171ce7b12
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-04 16:33:16 +0300

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  a = a.replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g, "").toLowerCase();
+  const noSymbols = a.replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g, "").toLowerCase();
+  const reveseString = noSymbols.split("").reverse().join("");
+  if (reveseString === a.toLowerCase()) return true;
+  else return false;
 };
 
 // Do not edit below this line
