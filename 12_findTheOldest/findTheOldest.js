7bd78e7ce5e16d102c9c87422d122b6cc4ad46ee
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-05 17:35:56 +0300

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentPersonAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
 
+    return oldestAge < currentPersonAge ? currentPerson : oldest;
+  });
+
+  function getAge(death, birth) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 };
 
 // Do not edit below this line
